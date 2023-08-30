6b79bf7b1122622462e13384bec8e82029c68d98
CChimene <coby@chimene.com>
2023-08-30 12:21:40 -0600

    Handler creation
    Created a simple handler which checks that the file is not empty and of the correct format.

--- Models/PayrollRecord.cs
+++ Models/PayrollRecord.cs
@@ -3,5 +3,5 @@ public class PayrollRecord
     public int EmployeeID { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string PayrollError { get; set; }
+    public bool PayrollError { get; set; }
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ConstrainedExecution;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 namespace ConroeISDInterview.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public string Message {get; set;} = "Initial Value";
+    public readonly long emptyFileSize = 48;
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -16,4 +25,23 @@ public class IndexModel : PageModel
     {
 
     }
+
+    public void OnPostUpload(IFormFile fileName){
+        try{
+            if(fileName.Length <= emptyFileSize){
+                Message ="File does not contain any records";
+            }
+            else if(!fileName.ContentType.Equals("text/csv")){
+                Message = "File is not in the correct format (.csv)";
+            }
+            else{
+                Message = "File is good";
+            }
+
+        }
+        catch {
+
+        }
+    }
+
 }
