3dbfb70dd938a420e14ac75213de14fb2cfc878d
CChimene <coby@chimene.com>
2023-08-30 10:19:14 -0600

    Basic Setup
    Added a PayrollRecord class for the database model and the MVC6 Grid Library. Added a standard file input and submit button to the main page.

---
+++ Models/PayrollRecord.cs
@@ -0,0 +1,7 @@
+public class PayrollRecord
+{
+    public int EmployeeID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PayrollError { get; set; }
+}
